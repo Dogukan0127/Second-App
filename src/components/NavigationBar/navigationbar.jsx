f10b7ab5b18ce8af0449675edd4666621bacd99b
Dogukan0127 <dogukangs2005@gmail.com>
2021-10-10 18:23:02 +0300

    first commit

---
+++ src/components/NavigationBar/navigationbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavigationBar() {
+    return (
+        <nav>
+            <ul className="list">
+                <li>Logo</li>
+                <li>HomePage</li>
+                <li>Content</li>
+                <li>Videos</li>
+                <li>Pictures</li>
+            </ul>
+        </nav>
+    )
+}
